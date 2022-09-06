87caff5a4afb00090f5f384bde740e1b86a5af47
Denis Baryshev <dennybaa@gmail.com>
2022-09-07 00:06:47 +0300

    add outputs and debug
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- src/workflow_dispatch.ts
+++ src/workflow_dispatch.ts
@@ -11,19 +11,21 @@ export interface WorkflowDispatchOpts {
   ref: string
 }
 
+// Dispatches a workflow and returns its id
 export async function workflow_dispatch(
   opts: WorkflowDispatchOpts,
   inputs: { [k: string]: string }
-): Promise<void> {
+): Promise<number> {
   const ref = opts.ref || github.context.ref
   const [owner, repo] = opts.repo
     ? opts.repo.split('/')
     : [github.context.repo.owner, github.context.repo.repo]
 
   const octokit = new Octokit({ auth: opts.token })
-
+  let found
   type WorkflowsResponseList =
     Endpoints['GET /repos/{owner}/{repo}/actions/workflows']['response']['data']['workflows']
+
   const workflows: WorkflowsResponseList = await octokit.paginate(
     octokit.rest.actions.listRepoWorkflows.endpoint.merge({
       owner,
@@ -33,26 +35,35 @@ export async function workflow_dispatch(
     })
   )
 
-  let found
   for (const w of workflows) {
     if (w.name === opts.workflow || w.id.toString() === opts.workflow) {
       found = w
+      core.debug(`Dispatched workflow ${opts.workflow} (${found.id})`)
       break
     }
   }
 
   if (!found) {
     throw new Error(
-      `Unable to find workflow '${opts.workflow}' in ${owner}/${repo} 😥`
+      `Unable to find workflow: '${opts.workflow}' 😥`
     )
   }
-  core.debug(`Dispatch workflow ${opts.workflow} (${found.id})`)
-  const dispatchResp = await octokit.request(
-    `POST /repos/${owner}/${repo}/actions/workflows/${found.id}/dispatches`,
+
+  // Dispatch the workflow run
+  const dispatchResp = await octokit.request(`POST /repos/${owner}/${repo}/actions/workflows/${found.id}/dispatches`,
     {
       ref,
       inputs
     }
   )
-  core.info(`Dispatch workflow status: ${dispatchResp.status} 🚀`)
+
+  if (dispatchResp.status === 204) {
+    core.info(`Successfully dispatched workflow: ${found.name} 🚀`)
+    return found.id
+  }
+  else {
+    throw new Error(
+      `Workflow ${found.name} dispatch failed, status: ${dispatchResp.status} 😥`
+    )
+  }
 }
