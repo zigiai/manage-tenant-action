1f65962c14888fae589904a8479f5c56c17d2923
Denis Baryshev <dennybaa@gmail.com>
2022-09-07 17:43:08 +0300

    add workflow run id detection
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- src/workflow_dispatch.ts
+++ src/workflow_dispatch.ts
@@ -3,67 +3,135 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { Endpoints } from '@octokit/types' // eslint-disable-line import/named
 import { Octokit } from '@octokit/rest'
+import { StringMap } from './tenants'
 
-export interface WorkflowDispatchOpts {
-  token: string
-  workflow: string
-  repo: string
-  ref: string
+// Arbitary string interface
+export interface CallOpts {
+  [key: string]: string | string[] | undefined | Octokit
 }
 
-// Dispatches a workflow and returns its id
-export async function workflow_dispatch(
-  opts: WorkflowDispatchOpts,
-  inputs: { [k: string]: string }
-): Promise<number> {
+export interface Caller extends CallOpts {
+  octokit: Octokit
+}
+
+// Accessed API endpoints
+type WorkflowsList =
+  Endpoints['GET /repos/{owner}/{repo}/actions/workflows']['response']['data']['workflows']
+type WorkflowsRunsList =
+  Endpoints['GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs']['response']['data']['workflow_runs']
+
+// Return a caller object
+function initCaller(opts: CallOpts): Caller {
+  const octokit = new Octokit({ auth: opts.token })
+  const caller: Caller = { octokit }
+
+  // Normalize owner, repo and ref (if any)
   const ref = opts.ref || github.context.ref
   const [owner, repo] = opts.repo
-    ? opts.repo.split('/')
+    ? (opts.repo as string).split('/')
     : [github.context.repo.owner, github.context.repo.repo]
 
-  const octokit = new Octokit({ auth: opts.token })
-  let found
-  type WorkflowsResponseList =
-    Endpoints['GET /repos/{owner}/{repo}/actions/workflows']['response']['data']['workflows']
+  Object.assign(caller, opts, { owner, repo, ref })
+  return caller
+}
 
-  const workflows: WorkflowsResponseList = await octokit.paginate(
-    octokit.rest.actions.listRepoWorkflows.endpoint.merge({
-      owner,
-      repo,
-      ref,
-      inputs
+// Find a workflow in the repo
+async function getWorkflow(caller: Caller): Promise<WorkflowsList[0]> {
+  let found
+  const workflows: WorkflowsList = await caller.octokit.paginate(
+    caller.octokit.rest.actions.listRepoWorkflows.endpoint.merge({
+      owner: caller.owner,
+      repo: caller.repo
     })
   )
 
   for (const w of workflows) {
-    if (w.name === opts.workflow || w.id.toString() === opts.workflow) {
+    if (w.name === caller.workflow || w.id.toString() === caller.workflow) {
       found = w
-      core.debug(`Dispatched workflow ${opts.workflow} (${found.id})`)
+      core.debug(`Dispatched workflow ${caller.workflow} (${found.id})`)
       break
     }
   }
 
   if (!found) {
-    throw new Error(
-      `Unable to find workflow: '${opts.workflow}' 😥`
-    )
+    throw new Error(`Unable to find workflow: '${caller.workflow}' 😥`)
   }
 
-  // Dispatch the workflow run
-  const dispatchResp = await octokit.request(`POST /repos/${owner}/${repo}/actions/workflows/${found.id}/dispatches`,
+  return found
+}
+
+async function getLatestRuns(
+  workflow: WorkflowsList[0],
+  caller: Caller,
+  latest_number = 10
+): Promise<WorkflowsRunsList> {
+  const options = caller.octokit.rest.actions.listWorkflowRuns.endpoint.merge({
+    owner: caller.owner,
+    repo: caller.repo,
+    workflow_id: workflow.id,
+    per_page: latest_number
+  })
+
+  const runs = await caller.octokit.paginate(options, (response, done) => {
+    done()
+    return response.data as WorkflowsRunsList
+  })
+
+  return runs
+}
+
+// Wait for queued
+async function waitForQueued(
+  workflow: WorkflowsList[0],
+  headRun: WorkflowsRunsList[0],
+  caller: Caller
+): Promise<WorkflowsRunsList[0] | undefined> {
+  const expected = ['in_progress', 'queued', 'requested', 'waiting']
+  let run_number = 0
+  if (headRun) {
+    run_number = headRun.run_number
+  }
+
+  for (let i = 0; i < 10; i++) {
+    // sleep for a second
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    const runs = await getLatestRuns(workflow, caller, 10)
+
+    // Find the first queued work
+    for (const run of runs) {
+      if (run.event !== 'workflow_dispatch' || run.status == null) continue
+      if (run.run_number > run_number && expected.includes(run.status)) {
+        return run
+      }
+    }
+  }
+}
+
+// Dispatches a workflow and returns the dispatched workflow run id
+export async function workflow_dispatch(
+  opts: CallOpts,
+  inputs: StringMap
+): Promise<number | undefined> {
+  const caller: Caller = initCaller(opts)
+  const workflow = await getWorkflow(caller)
+
+  // Get the N==1 list of recent workflow runs before dispatch
+  const headRun = (await getLatestRuns(workflow, caller, 1))[0]
+
+  // Dispatch run
+  const dispatchResp = await caller.octokit.request(
+    `POST /repos/${caller.owner}/${caller.repo}/actions/workflows/${workflow.id}/dispatches`,
     {
-      ref,
+      ref: caller.ref,
       inputs
     }
   )
 
-  if (dispatchResp.status === 204) {
-    core.info(`Successfully dispatched workflow: ${found.name} 🚀`)
-    return found.id
-  }
-  else {
+  if (dispatchResp.status !== 204) {
     throw new Error(
-      `Workflow ${found.name} dispatch failed, status: ${dispatchResp.status} 😥`
+      `Workflow ${workflow.name} dispatch failed, status: ${dispatchResp.status} 😥`
     )
   }
+
+  return (await waitForQueued(workflow, headRun, caller))?.id
 }
